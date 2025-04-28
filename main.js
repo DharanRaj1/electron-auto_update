ca2ab03e16cb23af8766d5f3d8547542f488b062
DharanRaj1 <dharan.raj@iamneo.ai>
2025-04-28 19:00:52 +0530

    update to force reinstall

--- main.js
+++ main.js
@@ -2,26 +2,30 @@ const { app, BrowserWindow, ipcMain, ipcRenderer } = require("electron");
 const MainScreen = require("./screens/main/mainScreen");
 const Globals = require("./globals");
 const { autoUpdater, AppUpdater } = require("electron-updater");
-
 let curWindow;
 
-//Basic flags
+// Configure for full reinstallation
 autoUpdater.autoDownload = false;
 autoUpdater.autoInstallOnAppQuit = true;
+
+// Add this line to force full package replacement
+autoUpdater.forceDevUpdateConfig = true;
+
+// Set allowDowngrade to true if you want to ensure any version can replace the current one
+autoUpdater.allowDowngrade = true;
 
 function createWindow() {
   curWindow = new MainScreen();
 }
 
-app.whenReady().then(async () => {
+app.whenReady().then(() => {
   createWindow();
 
   app.on("activate", function () {
     if (BrowserWindow.getAllWindows().length == 0) createWindow();
   });
-
-  let resp = await autoUpdater.checkForUpdates();
-  curWindow.showMessage(`Checking for updates. Current version ${app.getVersion()}\n${resp}`);
+  autoUpdater.checkForUpdatesAndNotify();
+  curWindow.showMessage(`Checking for updates. Current version ${app.getVersion()}`);
 });
 
 /*New Update Available*/
@@ -38,15 +42,13 @@ autoUpdater.on("update-not-available", (info) => {
 /*Download Completion Message*/
 autoUpdater.on("update-downloaded", (info) => {
   curWindow.showMessage(`Update downloaded. Current version ${app.getVersion()}`);
-  autoUpdater.quitAndInstall();
+  // Force reinstall with isForceRunAfter set to true
+  autoUpdater.quitAndInstall(false, true);
 });
 
 autoUpdater.on("error", (info) => {
   curWindow.showMessage(info);
 });
-
-
-
 
 //Global exception handler
 process.on("uncaughtException", function (err) {

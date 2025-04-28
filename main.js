291e81fa6f61c8affdf304fa6d0175025ccc47b1
DharanRaj1 <dharan.raj@iamneo.ai>
2025-04-28 13:28:49 +0530

    main files

---
+++ main.js
@@ -0,0 +1,21 @@
+const { app, BrowserWindow, autoUpdater } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+  });
+
+  win.loadURL('data:text/html,<h1>Hello! This is Version 1.0.0</h1>');
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  autoUpdater.checkForUpdatesAndNotify();
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
